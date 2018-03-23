6ba9a6a5a677d5c1ae7caa3ecb30ca69948eb522
kahee <yuygh131@gmail.com>
2018-03-24 06:37:06 +0900

    db  저장

--- app/search_flight/views.py
+++ app/search_flight/views.py
@@ -7,23 +7,37 @@ from django.shortcuts import render, get_object_or_404, render_to_response
 from django.template import RequestContext
 
 from crawler.expedia import get_ticket_information
-from search_flight.models import City_Infomation
+from search_flight.models import City_Infomation, TicketInfomation
 
 from django.views.decorators.csrf import csrf_exempt
 
 
 @csrf_exempt
 def add_flight(request):
-    if request.method == 'POST':
-        info = request.POST['info']
-        info[0]
-
-        ticket, _ =  TicketInfomation.objects.get_or_create(
-
-        )
+    pass
 
 
-        return HttpResponse(info)
+#     if request.method == 'POST':
+#         startcity = request.POST['startcity']
+#         arrivecity = request.POST['arrivecity']
+#         startdate = request.POST['startdate']
+#         enddate = request.POST['enddate']
+#         price = request.POST['price']
+#         print(startcity,arrivecity,startdate,enddate,price)
+#
+#
+#         # db저장할 때
+#
+#
+#         # ticket, _ = TicketInfomation.objects.get_or_create(
+#         #     startcity=info[0]['startcity'],
+#         #     arrivecity=info[0]['endcity'],
+#         #     startdate=info[0]['stardate'],
+#         #     enddate=info[0]['enddate'],
+#         #     price=info[0]['price'],
+#         # )
+#
+#         return HttpResponse(info)
 
 
 @csrf_exempt
@@ -44,10 +58,6 @@ def SearchFlight(request):
         startcity = City_Infomation.objects.get(pk=startcity_info).get_cityname_display()
         arrivecity = City_Infomation.objects.get(pk=arrivecity_info).get_cityname_display()
 
-        # db저장할 때
-        startdate = datetime.strptime(startdate_info, '%Y.%m.%d')
-        enddate = datetime.strptime(enddate_info, '%Y.%m.%d')
-
         result = get_ticket_information(startdate_info, enddate_info, startcity, arrivecity)
 
         start = result[0]
@@ -57,16 +67,16 @@ def SearchFlight(request):
         price = start['출발']['price'] + arrive['도착']['price']
 
         if int(price_info) >= price:
-            info = list()
-            info.append({
-                'startcity': startcity,
-                'endcity': arrivecity,
-                'stardate': startdate,
-                'enddate': enddate,
-                'price': price,
-            })
+            startdate = datetime.strptime(startdate_info, '%Y.%m.%d')
+            enddate = datetime.strptime(enddate_info, '%Y.%m.%d')
+            ticket, _ = TicketInfomation.objects.get_or_create(
+                startcity=City_Infomation.objects.get(pk=startcity_info),
+                arrivecity=City_Infomation.objects.get(pk=arrivecity_info),
+                startdate=startdate,
+                enddate=enddate,
+                price=price,
+            )
 
-            context['info'] = info
             context['start_time'] = start['출발']['time']
             context['arrive_time'] = arrive['도착']['time']
             context['price'] = price
