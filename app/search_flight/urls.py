ccd79b7c00fa33b0460214446fbb22d9db15072e
kahee <yuygh131@gmail.com>
2018-03-24 05:52:58 +0900

    add urls 작성

--- app/search_flight/urls.py
+++ app/search_flight/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import SearchFlight
+from .views import SearchFlight, add_flight
 
 urlpatterns = [
-    path('search/', SearchFlight, name='search-flight')
+    path('search/', SearchFlight, name='search-flight'),
+    path('add/', add_flight, name='add-flight')
 ]
--- app/search_flight/views.py
+++ app/search_flight/views.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, render_to_response
 
 # Create your views here.
@@ -10,16 +11,31 @@ from search_flight.models import City_Infomation
 
 from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
+def add_flight(request):
+    if request.method == 'POST':
+        info = request.POST['info']
+        info[0]
+
+        ticket, _ =  TicketInfomation.objects.get_or_create(
+
+        )
+
+
+        return HttpResponse(info)
+
+
 @csrf_exempt
 def SearchFlight(request):
     context = dict()
 
     city = City_Infomation.objects.all()
+
     if request.method == 'POST':
 
         startcity_info = request.POST['startcity']
         arrivecity_info = request.POST['arrivecity']
-        print(startcity_info , arrivecity_info)
 
         startdate_info = request.POST['startdate']
         enddate_info = request.POST['enddate']
@@ -28,28 +44,33 @@ def SearchFlight(request):
         startcity = City_Infomation.objects.get(pk=startcity_info).get_cityname_display()
         arrivecity = City_Infomation.objects.get(pk=arrivecity_info).get_cityname_display()
 
-
-        stardate = datetime.strptime(startdate_info, '%Y.%m.%d')
+        # db저장할 때
+        startdate = datetime.strptime(startdate_info, '%Y.%m.%d')
         enddate = datetime.strptime(enddate_info, '%Y.%m.%d')
 
-<<<<<<< HEAD
-        result = get_ticket_information(startdate_info, enddate_info, startcity, a)
-=======
         result = get_ticket_information(startdate_info, enddate_info, startcity, arrivecity)
-
-
->>>>>>> 5e5156afaa880aea1294e70eea8200b7617c8abf
 
         start = result[0]
         arrive = result[1]
 
+        print(start['출발']['time'])
         price = start['출발']['price'] + arrive['도착']['price']
 
         if int(price_info) >= price:
-            context['result'] = result
+            info = list()
+            info.append({
+                'startcity': startcity,
+                'endcity': arrivecity,
+                'stardate': startdate,
+                'enddate': enddate,
+                'price': price,
+            })
+
+            context['info'] = info
+            context['start_time'] = start['출발']['time']
+            context['arrive_time'] = arrive['도착']['time']
+            context['price'] = price
             return render(request, 'index.html', context)
 
     context['items'] = city
-    # return render(request, 'index.html', {'items': city})
     return render_to_response('index.html', context)
-    # return render(request,'index.html')
