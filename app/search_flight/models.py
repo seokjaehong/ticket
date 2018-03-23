9b4ea2b55c1061c3e21205cfe71b81e28fadd457
seokjae hong <hsj2334@gmail.com>
2018-03-24 03:29:30 +0900

    index.html 수정중, model에 city class추가, bootstrap추가

--- app/search_flight/models.py
+++ app/search_flight/models.py
@@ -7,4 +7,22 @@ class TicketInfomation(models.Model):
     arrivecity = models.CharField('도착도시', max_length=200, blank=True)
     startdate = models.DateField('출발날짜', )
     enddate = models.DateField('도착날짜', )
-    price = models.CharField('가격',max_length=200,)
+    price = models.CharField('가격', max_length=200, )
+
+
+class City_Infomation(models.Model):
+    CITY_CODE_SEOUL = 'SEL'
+    CITY_CODE_PUSAN = 'PUS'
+    CITY_CODE_INCHEON = 'ICN'
+    CITY_CODE_GIMPO = 'GMP'
+    CITY_CODE_ROMA = 'ROM'
+    CITY_CODE_FLORENCE = 'FLR'
+    CHOICES_BLOOD_TYPE = (
+        (CITY_CODE_SEOUL, '서울'),
+        (CITY_CODE_PUSAN, '부산'),
+        (CITY_CODE_INCHEON, '인천'),
+        (CITY_CODE_GIMPO, '김포'),
+        (CITY_CODE_ROMA, '로마'),
+        (CITY_CODE_FLORENCE, '피렌체'),
+    )
+    cityname = models.CharField('도시명', max_length=3, choices=CHOICES_BLOOD_TYPE, blank=True)
