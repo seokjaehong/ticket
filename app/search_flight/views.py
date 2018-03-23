b235632683812383f3a698a3440a1a2920c42a9f
kahee <yuygh131@gmail.com>
2018-03-24 03:54:43 +0900

    expedia 비행시간에서 +1 되는 부분 \n 삭제하고 출력

--- app/search_flight/views.py
+++ app/search_flight/views.py
@@ -19,7 +19,7 @@ def SearchFlight(request):
         stardate = datetime.strptime(startdate_info, '%Y.%m.%d')
         enddate = datetime.strptime(enddate_info, '%Y.%m.%d')
 
-        result = get_ticket_information(startdate_info, enddate_info, 'SEL', 'ROM')
+        result = get_ticket_information(startdate_info, enddate_info, 'SEL', 'PUS')
 
         start = result[0]
         arrive = result[1]
