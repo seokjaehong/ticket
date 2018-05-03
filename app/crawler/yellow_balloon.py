09f9cd934de1ca36fc3627a4ac33282f354ac9e7
seokjae hong <hsj2334@gmail.com>
2018-05-03 21:50:44 +0900

    비행기티켓 모델링 수정

---
+++ app/crawler/yellow_balloon.py
@@ -0,0 +1,14 @@
+
+def checkcity(start, arrive):
+    city = dict()
+    city['서울'] = 'ICN,GMP'
+    city['로마'] = 'FCO,CIA,IRT,XRJ'
+    city['피렌체'] = 'FLR,FIR,ZMS'
+    city['프라하'] = 'PRG,XYG'
+    return city[start], city[arrive]
+
+
+def searchFlight(url, start=False):
+    driver = webdriver.Chrome('chromedriver')
+    driver.implicitly_wait(6)
+    driver.get(url)
