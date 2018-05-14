b03dd7a5efde02f2e78eada176c16bbdc6eff509
hong <hsj2334@gmail.com>
2018-05-14 18:47:56 +0900

    노랑풍선 크롤링 stop

--- app/crawler/expedia.py
+++ app/crawler/expedia.py
@@ -1,51 +1,56 @@
 from re import sub
 
 from selenium import webdriver
+
 driver = webdriver.Chrome('chromedriver')
 
-startdate = '2018.05.21'
-enddate = '2018.05.24'
-startcity = 'SEL'
-endcity = "PUS"
+departure_date = '2018.05.17'
+arrival_date = '2018.05.19'
+departure_city = 'SEL'
+arrival_city = "PUS"
 
 driver.implicitly_wait(3)
-url = "https://www.expedia.co.kr/Flights-Search?flight-type=on&starDate=" + startdate + \
-      "&endDate=" + enddate + "" \
-                              "&_xpid=11905%7C1" \
-                              "&mode=search" \
-                              "&trip=roundtrip" \
-                              "&leg1=from%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
-      + startcity + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cto%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28" \
-      + endcity + "-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
-      + startdate + "TANYT&leg2=from%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28PUS-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cto%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28SEL-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
-      + enddate + "TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
+url = "https://www.expedia.co.kr/Flights-Search?flight-type=on" \
+      "&starDate=" + departure_date + \
+      "&endDate=" + arrival_date + \
+      "&_xpid=11905%7C1" \
+      "&mode=search" \
+      "&trip=roundtrip" \
+      "&leg1=from%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
+      + departure_city + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cto%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28" \
+      + arrival_city + "-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
+      + departure_date + "TANYT&leg2=from%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28" \
+      + arrival_city + "-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cto%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
+      + departure_city + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
+      + arrival_date + "TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
 
 driver.get(url)
+def get_flight_time_from_webpage(obj):
+    time_info = obj.find_element_by_class_name('flight-module').find_element_by_class_name(
+            'primary-content')
+    return time_info
 
+def get_flight_price_from_webpage(obj):
+    price_raw = obj.find_element_by_class_name('grid-container').find_element_by_class_name(
+        'all-col-shrink').find_element_by_class_name('primary-content')
+
+    price_info = int(sub(r'[^\d.]', '', price_raw.text))
+    return price_info
 
 def get_ticket_information():
-    def get_time_price_from_home_page():
-        time_info = flight_infomation.find_element_by_class_name('flight-module').find_element_by_class_name(
-            'primary-content')
-        price_raw = flight_infomation.find_element_by_class_name('grid-container').find_element_by_class_name(
-            'all-col-shrink').find_element_by_class_name('primary-content')
+    flight_infomations = driver.find_element_by_id('flight-listing-container')
 
-        price_info = int(sub(r'[^\d.]', '', price_raw.text))
-        result = {time_info.text, price_info}
-        return result
+    for flight in flight_infomations :
+        departure_time = get_flight_time_from_webpage(flight)
+        departure_price = get_flight_price_from_webpage(flight)
 
-    flight_infomation = driver.find_element_by_id('flight-listing-container')
+        flight.find_element_by_class_name('grid-container').find_element_by_class_name(
+            'all-col-shrink').find_element_by_class_name('standard-col-l-margin').click()
+        driver.implicitly_wait(5)
 
-    departure_info = get_time_price_from_home_page()
+        arrival_time = get_flight_time_from_webpage(flight_infomation)
+        arrival_price = get_flight_price_from_webpage(flight_infomation)
 
-    flight_infomation.find_element_by_class_name('grid-container').find_element_by_class_name(
-        'all-col-shrink').find_element_by_class_name('standard-col-l-margin').click()
-    driver.implicitly_wait(5)
-
-    arrival_info = get_time_price_from_home_page()
-
-    result = departure_info, arrival_info
-    return result
 
 
 result = get_ticket_information()
--- app/crawler/yellow_balloon.py
+++ app/crawler/yellow_balloon.py
@@ -1,16 +1,25 @@
-from selenium.webdriver.chrome import webdriver
 
+from selenium import webdriver
 
-def checkcity(start, arrive):
-    city = dict()
-    city['서울'] = 'ICN,GMP'
-    city['로마'] = 'FCO,CIA,IRT,XRJ'
-    city['피렌체'] = 'FLR,FIR,ZMS'
-    city['프라하'] = 'PRG,XYG'
-    return city[start], city[arrive]
-
-
-def searchFlight(url, start=False):
-    driver = webdriver.Chrome('chromedriver')
-    driver.implicitly_wait(6)
-    driver.get(url)
+driver = webdriver.Chrome('chromedriver')
+url = "http://air.ybtour.co.kr/air/b2c/AIR/MBL/AIRMBLSCH0100100010.k1?initform=RT&domintgubun=I" \
+      "&depctycd=SEL" \
+      "&depctycd=NRT" \
+      "&depctycd=&depctycd=" \
+      "&arrctycd=NRT" \
+      "&arrctycd=SEL" \
+      "&arrctycd=&arrctycd=&depctynm=%EC%9D%B8%EC%B2%9C%2F%EA%B9%80%ED%8F%AC&depctynm=%EB%8F%84%EC%BF%84%2F%EB%82%98%EB%A6%AC%ED%83%80&depctynm=&depctynm=&arrctynm=%EB%8F%84%EC%BF%84%2F%EB%82%98%EB%A6%AC%ED%83%80&arrctynm=%EC%9D%B8%EC%B2%9C%2F%EA%B9%80%ED%8F%AC&arrctynm=&arrctynm=" \
+      "&depdt=2018-05-18" \
+      "&depdt=2018-05-25" \
+      "&depdt=&depdt=" \
+      "&adtcount=1" \
+      "&chdcount=0" \
+      "&infcount=0&cabinclass=Y&preferaircd=&availcount=250&opencase=N&opencase=N&opencase=N&openday=&openday=&openday=&depdomintgbn=D&tasktype=B2C" \
+      "&secrchType=FARE&maxprice=&servicecacheyn=Y&skplt=N&areacd=&KSESID=air%3Ab2c%3ASELK138AN%3AAA024%3A%3A00"
+driver.get(url)
+driver.implicitly_wait(5)
+table_list = driver.find_element_by_xpath("//*[@id='div_fare']/tbody")
+company = table_list.find_element_by_xpath("tr[1]/td[1]/ul/li[2]/strong").text
+price = table_list.find_element_by_xpath("tr[1]/td[2]/span[1]").text
+print(company)
+print(price)
