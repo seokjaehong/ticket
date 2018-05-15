9429f041c8f7f7d5dc9f6e1d3f64b4960e768dec
hong <hsj2334@gmail.com>
2018-05-15 23:54:32 +0900

    basehtml 수정

--- app/config/settings.py
+++ app/config/settings.py
@@ -13,10 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 ROOT_DIR = os.path.dirname(BASE_DIR)
-print(ROOT_DIR)
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+TEMPLATE_DIR = os.path.join(ROOT_DIR, 'templates')
 print(TEMPLATE_DIR)
 
 # Quick-start development settings - unsuitable for production
