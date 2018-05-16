95abcbe23809748b645ebb45db8488a68ec0d926
hong <hsj2334@gmail.com>
2018-05-17 02:53:40 +0900

    모델 수정, 크롤링 수정(왕복->편도)로 변경)

--- app/ticket/crawler/expedia.py
+++ app/ticket/crawler/expedia.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from datetime import datetime
+from datetime import timedelta
 from re import sub
 
 from selenium import webdriver
@@ -11,93 +12,126 @@ import django
 django.setup()
 
 
-
 class TicketDetailData():
-    def get_flight_time_from_webpage(self, obj):
-        time_info = obj.find_element_by_class_name('flight-module').find_element_by_class_name(
-            'custom-primary-padding').text
-        return time_info
-
-    def get_flight_price_from_webpage(self, obj):
-        price_raw = obj.find_element_by_class_name('grid-container').find_element_by_class_name(
-            'all-col-shrink').find_element_by_class_name('primary-content')
-        price_info = int(sub(r'[^\d.]', '', price_raw.text))
-
-        return price_info
+    # def get_flight_time_from_webpage(self, obj):
+    #     time_info = obj.find_element_by_class_name('flight-module').find_element_by_class_name(
+    #         'custom-primary-padding').text
+    #     print(time_info)
+    #     return time_info
+    #
+    # def get_flight_price_from_webpage(self, obj):
+    #     price_raw = obj.find_element_by_class_name('grid-container').find_element_by_class_name(
+    #         'all-col-shrink').find_element_by_class_name('primary-content')
+    #     price_info = int(sub(r'[^\d.]', '', price_raw.text))
+    #     print(price_info)
+    #     return price_info
+    departure_date = '2018.05.26'
 
     def get_ticket_information(self):
         driver = webdriver.Chrome('chromedriver')
 
-        departure_date = '2018.05.17'
-        arrival_date = '2018.05.19'
-        departure_city = 'SEL'
-        arrival_city = "PUS"
-
-        url = "https://www.expedia.co.kr/Flights-Search?flight-type=on" \
-              "&starDate=" + departure_date + \
-              "&endDate=" + arrival_date + \
-              "&_xpid=11905%7C1" \
-              "&mode=search" \
-              "&trip=roundtrip" \
-              "&leg1=from%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
-              + departure_city + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cto%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28" \
-              + arrival_city + "-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
-              + departure_date + "TANYT&leg2=from%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28" \
-              + arrival_city + "-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cto%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
-              + departure_city + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
-              + arrival_date + "TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
+        origin_place = 'ICN'
+        destination_place = "ROM"
+        url = "https://www.expedia.co.kr/Flights-Search?flight-type=on&starDate=" \
+              + self.departure_date + "&_xpid=11905%7C1&mode=search&trip=oneway&leg1=from%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
+              + origin_place + "-%EC%9D%B8%EC%B2%9C%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cto%3A%EB%A1%9C%EB%A7%88%2C+%EC%9D%B4%ED%83%88%EB%A6%AC%EC%95%84+%28" \
+              + destination_place + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
+              + self.departure_date + "TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
+        # departure_date = '2018.05.25'
+        # arrival_date = '2018.06.01'
+        # departure_city = 'ICN'
+        # arrival_city = "PUS"
+        #
+        # url = "https://www.expedia.co.kr/Flights-Search?flight-type=on" \
+        #       "&starDate=" + departure_date + \
+        #       "&endDate=" + arrival_date + \
+        #       "&_xpid=11905%7C1" \
+        #       "&mode=search" \
+        #       "&trip=roundtrip" \
+        #       "&leg1=from%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
+        #       + departure_city + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cto%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28" \
+        #       + arrival_city + "-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
+        #       + departure_date + "TANYT&leg2=from%3A%EB%B6%80%EC%82%B0%2C+%ED%95%9C%EA%B5%AD+%28" \
+        #       + arrival_city + "-%EA%B9%80%ED%95%B4%EA%B5%AD%EC%A0%9C%EA%B3%B5%ED%95%AD%29%2Cto%3A%EC%84%9C%EC%9A%B8%2C+%ED%95%9C%EA%B5%AD+%28" \
+        #       + departure_city + "-%EB%AA%A8%EB%93%A0+%EA%B3%B5%ED%95%AD%29%2Cdeparture%3A" \
+        #       + arrival_date + "TANYT&passengers=children%3A0%2Cadults%3A1%2Cseniors%3A0%2Cinfantinlap%3AY"
 
         driver.get(url)
-        driver.implicitly_wait(5)
+        driver.implicitly_wait(3)
 
-        flight_informations = driver.find_elements_by_id('flightModuleList')
+        # flight_informations = driver.find_elements_by_id('flight-listing-container')
+        flight_informations = driver.find_elements_by_class_name('flight-module')
+
         result = []
+        i = 0
+        # new_departure_date = datetime.strftime(datetime.strptime(self.departure_date, '%Y.%m.%d'), '%Y-%m-%d')
+        new_departure_date = datetime.strptime(self.departure_date,'%Y.%m.%d')
 
         for flight in flight_informations:
-            departure_time = self.get_flight_time_from_webpage(flight)
-            departure_price = self.get_flight_price_from_webpage(flight)
 
-            flight.find_element_by_class_name('grid-container').find_element_by_class_name(
-                'all-col-shrink').find_element_by_class_name('standard-col-l-margin').click()
-            driver.implicitly_wait(5)
+            flight_info = flight.find_element_by_css_selector(
+                "div > div.uitk-grid.all-grid-fallback-alt > div.uitk-col.all-col-fill.custom-short-r-margin > div")
+            flight_departure_datetime = flight_info.find_element_by_css_selector(
+                "div > div > div.uitk-col.tablet-col-1-3.desktop-col-1-3.custom-col-r-margin.min-width-large-screens-only > div.primary-content.no-wrap.custom-primary-padding > span > span:nth-child(1)").text
+            flight_arrival_datetime = flight_info.find_element_by_css_selector(
+                "div > div > div.uitk-col.tablet-col-1-3.desktop-col-1-3.custom-col-r-margin.min-width-large-screens-only > div.primary-content.no-wrap.custom-primary-padding > span > span:nth-child(2)").text
+            flight_company = flight_info.find_element_by_css_selector(
+                "div > div > div.uitk-col.tablet-col-1-3.desktop-col-1-3.custom-col-r-margin.min-width-large-screens-only > div.secondary-content.overflow-ellipsis.inline-children > span").text
+            flight_time = flight_info.find_element_by_css_selector(
+                "div > div > div.uitk-col.tablet-col-1-2.desktop-col-1-2.all-col-fill > div.fluid-content.inline-children.custom-primary-padding > span.duration-emphasis").text
 
-            arrival_time = self.get_flight_time_from_webpage(flight)
-            arrival_price = self.get_flight_price_from_webpage(flight)
+            ##여기 수정할것. 
+            if flight_info.find_elements_by_css_selector(
+                "div > div > div.uitk-col.tablet-col-1-3.desktop-col-1-3.custom-col-r-margin.min-width-large-screens-only > div.primary-content.no-wrap.custom-primary-padding > span.primary-sub-content.urgency").count != 0:
+                arrival_date = new_departure_date + timedelta(days=1)
+            else :
+                arrival_date = self.departure_date
 
-        new_departure_date = datetime.strftime(datetime.strptime(departure_date,'%Y.%m.%d'),'%Y-%m-%d')
+            price_info = flight.find_element_by_css_selector(
+                'div > div.uitk-grid.all-grid-fallback-alt > div.uitk-col.all-col-shrink')
 
-        new_arrival_date = datetime.strftime(datetime.strptime(arrival_date,'%Y.%m.%d'),'%Y-%m-%d')
-        # new_arrival_date   = datetime.strftime(datetime.strftime(arrival_date,'%Y.%m.%d'),'%Y-%m-%d')
+            ##여기 수정할것.
+            price_raw = price_info.find_element_by_css_selector(
+                'div > div.uitk-col.custom-width.all-col-fill > div.primary-content').text
+            price = int(sub(r'[\d.*?^\d.]', '', price_raw))
 
-        result.append({
-            'goreturn': '1',
-            'departure_date': new_departure_date,
-            'departure_duration': departure_time,
-            'country': 'Korea',
-            'departure_city': departure_city,
-            'arrival_city': arrival_city,
-            'flight_company': '대한항공',
-            'ticket_price': departure_price,
-            'currency': 'test',
-            'fee_policy': 'test',
-            'data_source': 'expedia',
-            'url_link': 'test',
-        })
-        result.append({
-            'goreturn': '2',
-            'departure_date': new_arrival_date,
-            'departure_duration': arrival_time,
-            'country': 'Korea',
-            'departure_city': arrival_city,
-            'arrival_city': departure_city,
-            'flight_company': '대한항공',
-            'ticket_price': arrival_price,
-            'currency': 'test',
-            'fee_policy': 'test',
-            'data_source': 'expedia',
-            'url_link': 'test',
 
-        })
+            #  a tag 클릭해야함
+            # waypoint=flight.find_element_by_css_selector("#section-offer-leg0-details > div > div.layover-info > span.layover-city").text
+            # waypoint_duration = flight.find_element_by_css_selector("#section-offer-leg0-details > div > div.layover-info > span.layover-duration").text
+
+            # new_departure_date = datetime.strftime(datetime.strptime(departure_date, '%Y.%m.%d'), '%Y-%m-%d')
+            # price = self.get_flight_price_from_webpage(flight)
+            # flight.find_element_by_class_name()
+
+            # flight.find_element_by_class_name('grid-container').find_element_by_class_name(
+            #     'all-col-shrink').find_element_by_class_name('standard-col-l-margin').click()
+            # driver.implicitly_wait(5)
+            #
+            # arrival_time = self.get_flight_time_from_webpage(flight)
+            # arrival_price = self.get_flight_price_from_webpage(flight)
+
+            # new_arrival_date = datetime.strftime(datetime.strptime(arrival_date, '%Y.%m.%d'), '%Y-%m-%d')
+            # new_arrival_date   = datetime.strftime(datetime.strftime(arrival_date,'%Y.%m.%d'),'%Y-%m-%d')
+
+            result.append({
+                'origin_place': origin_place,
+                'destination_place': destination_place,
+                'is_direct': False,
+                'way_point': 'test',
+                'way_point_duration': 'testtime',
+                'ticket_price': price,
+                'departure_date': self.departure_date,
+                'departure_datetime': flight_departure_datetime,
+                'arrival_date': arrival_date,
+                'arrival_datetime': flight_arrival_datetime,
+                'flight_time': flight_time,
+                'flight_company': flight_company,
+                'currency': '환율',
+                'data_source': '익스피디아',
+                'url_link': 'http://www.naver.com',
+
+            })
 
         return result
 
@@ -112,16 +146,20 @@ if __name__ == '__main__':
     for ticket_data in ticket_datas:
         # 도시, 회사정보 저장
         city, _ = TicketData.objects.get_or_create(
-            goreturn=ticket_data['goreturn'],
-            departure_date=ticket_data['departure_date'],
-            departure_duration=ticket_data['departure_duration'],
-            country='Korea',
-            departure_city=ticket_data['departure_city'],
-            arrival_city=ticket_data['arrival_city'],
-            flight_company='대한항공',
+            origin_place=ticket_data['origin_place'],
+            destination_place=ticket_data['destination_place'],
+            is_direct=ticket_data['is_direct'],
+            way_point=ticket_data['way_point'],
+            way_point_duration=ticket_data['way_point_duration'],
             ticket_price=ticket_data['ticket_price'],
+
+            departure_date=ticket_data['departure_date'],
+            departure_datetime=ticket_data['departure_datetime'],
+            arrival_date=ticket_data['arrival_date'],
+            arrival_datetime=ticket_data['arrival_datetime'],
+
+            flight_company='대한항공',
             currency='test',
-            fee_policy='test',
             data_source='expedia',
-            # url_link='test',
+            url_link=ticket_data['url_link'],
         )
--- app/ticket/models.py
+++ app/ticket/models.py
@@ -7,22 +7,25 @@ __all__ = (
 
 # Create your models here.
 class TicketData(models.Model):
-    goreturn = models.CharField('출발/도착 구분', max_length=2)
-
-    departure_date = models.DateField('출발날짜', blank=True)
-    departure_duration = models.CharField('소요시간', max_length=100)
-    country = models.CharField('출발나라', max_length=50)
-    departure_city = models.CharField('출발도시', max_length=50)
-    arrival_city = models.CharField('도착도시', max_length=50)
-    flight_company = models.CharField('항공사명', max_length=20)
-
+    # ticketID = models.IntegerField('티켓ID', unique=True)
+    origin_place = models.CharField('출발도시', max_length=50)
+    destination_place = models.CharField('도착도시', max_length=50)
+    is_direct = models.BooleanField('경유여부', default=False)
+    way_point = models.CharField('경유지', max_length=50)
+    way_point_duration = models.CharField('경유시간', max_length=10)
     ticket_price = models.IntegerField('가격')
 
-    currency = models.CharField('환율', max_length=10, blank=True)
-    fee_policy = models.CharField('요금규정', max_length=100, blank=True)
+    departure_date = models.DateField('출발날짜')
+    departure_datetime = models.DateTimeField('출발시간')
 
+    arrival_date = models.DateField('도착날짜')
+    arrival_datetime = models.DateTimeField('도착시간')
+    flight_time = models.CharField('총소요시간', max_length=100)
+
+    flight_company = models.CharField('항공사명', max_length=20)
+    currency = models.CharField('환율', max_length=10, blank=True)
     data_source = models.CharField('데이터 출처', max_length=30)
-    # url_link = models.URLField('데이터 링크',blac)
+    url_link = models.URLField('웹페이지링크')
 
     is_delete = models.BooleanField('삭제데이터여부', default=False)
     create_datetime = models.DateTimeField('생성시간', auto_now_add=True)
