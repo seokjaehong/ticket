6f6ccbd9d64ffc035fee6bbac84e56aa14e17fae
hong <hsj2334@gmail.com>
2018-05-24 14:25:51 +0900

    tway 크롤링 편도티켓으로 변경

--- app/ticket/crawler/tway-air.py
+++ app/ticket/crawler/tway-air.py
@@ -26,15 +26,21 @@ class TwayData():
 
         flight_informations = driver.find_element_by_xpath(
             "//*[@id='header']/div[3]/div[2]/ul/li[1]/form/fieldset/div/div/ul")
+        #편도선택
+        flight_informations.find_element_by_xpath("//*[@id='header']/div[3]/div[2]/ul/li[1]/form/fieldset/div/ul/li[2]/label").click()
+        #노선선택
         flight_informations.find_element_by_xpath("li[1]/a").click()
+        #김포제주
         flight_informations.find_element_by_xpath("li[1]/section/div/dl[1]/dd[1]/a").click()
 
-        # 가는날자,오는날짜 선택, 사람수 선택, 확인버튼 클릭
+        # 가는날자, 사람수 선택, 확인버튼 클릭
         driver.find_element_by_xpath("//*[@id='onwardDatepicker']/div/div[2]/div/table/tbody/tr[4]/td[6]/a").click()
-        driver.find_element_by_xpath("//*[@id='returnDatepicker']/div/div[2]/div/table/tbody/tr[5]/td[4]/a").click()
+
+        # driver.find_element_by_xpath("//*[@id='returnDatepicker']/div/div[2]/div/table/tbody/tr[5]/td[4]/a").click()
         driver.find_element_by_xpath(
             "//*[@id='header']/div[3]/div[2]/ul/li[1]/form/fieldset/div/div/ul/li[2]/section/div/div[3]/div[1]/button").click()
         driver.implicitly_wait(1)
+        #//*[@id="header"]/div[3]/div[2]/ul/li[1]/form/fieldset/div/div/ul/li[2]/section/div/div[3]/div[1]/button
         driver.find_element_by_xpath(
             "//*[@id='header']/div[3]/div[2]/ul/li[1]/form/fieldset/div/div/ul/li[4]/section/div/p[3]/a").click()
         driver.maximize_window()
@@ -48,7 +54,7 @@ class TwayData():
         driver.implicitly_wait(2)
 
         departure_date = driver.find_element_by_xpath("//*[@id='resultbox1']/div[2]/ul/li[4]/a/span").text
-        arrival_date = driver.find_element_by_xpath("//*[@id='resultbox2']/div[2]/ul/li[4]/a/span").text
+        # arrival_date = driver.find_element_by_xpath("//*[@id='resultbox2']/div[2]/ul/li[4]/a/span").text
 
         ticket_informations = driver.find_elements_by_xpath("//*[@id='tbodyOnward']/tr")
 
@@ -72,7 +78,7 @@ class TwayData():
                 'ticket_price': price,
                 'departure_date': departure_date,
                 'departure_datetime': departure_datetime,
-                'arrival_date': arrival_date,
+                'arrival_date': departure_datetime,
                 'arrival_datetime': arrival_datetime,
                 'flight_time': flight_time,
                 'flight_company': 'tway',
