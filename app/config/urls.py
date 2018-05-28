7582799749950fc599e5a8d465311b0e55ce084b
hong <hsj2334@gmail.com>
2018-05-28 17:20:11 +0900

    serve-media view

--- app/config/urls.py
+++ app/config/urls.py
@@ -13,10 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+
+from config import settings
+from config.views import serve_media
 from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-]
+    # re_path(r'media/(?P<path>.*)$', serve_media),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/config/views.py
+++ app/config/views.py
@@ -1,7 +1,11 @@
+import mimetypes
+import os
+
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.shortcuts import render
 
+from config import settings
 from ticket.models import TicketData
 
 
@@ -14,4 +18,13 @@ def index(request):
     # return HttpResponse('Hello')
     # return render(request,'base.html',context)
     return render(request, 'base.html', context)
-#
+
+
+def serve_media(request, path):
+    media_path = os.path.join(settings.MEDIA_ROOT, path)
+    content_type = mimetypes.guess_type(path)
+    # return HttpResponse(media_path)
+    return FileResponse(
+        open(media_path, 'rb')
+        , content_type=content_type,
+    )
