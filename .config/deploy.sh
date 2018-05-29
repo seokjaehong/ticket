bdeaf35b4b573d7ef5954c0925a12d25adbcc1b8
hong <hsj2334@gmail.com>
2018-05-30 00:36:05 +0900

    collectstatic

--- .config/deploy.sh
+++ .config/deploy.sh
@@ -3,7 +3,12 @@ sudo rm -rf /etc/nginx/sites-enabled/*
 sudo cp -f /srv/ticket/.config/nginx-app.conf /etc/nginx/sites-available/nginx-app.conf
 sudo ln -sf /etc/nginx/sites-available/nginx-app.conf /etc/nginx/sites-enabled/nginx-app.conf
 sudo cp -f /srv/ticket/.config/uwsgi.service /etc/systemd/system/uwsgi.service
+cd /srv/ticket/app
+/bin/bash -c \
+'/home/ubuntu/.pyenv/versions/ticket/bin/python \
+/srv/ticket/app/manage.py collectstatic --noinput' ubuntu
 sudo systemctl enable uwsgi
 sudo systemctl daemon-reload
 sudo systemctl restart uwsgi nginx
+
 
