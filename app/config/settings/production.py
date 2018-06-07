162cc995f3b6aefcbbcda41b6e0695908f8dceb3
hong <hsj2334@gmail.com>
2018-06-07 19:17:23 +0900

    error 수정

--- app/config/settings/production.py
+++ app/config/settings/production.py
@@ -5,7 +5,10 @@ secrets = json.loads(open(SECRETS_PRODUCTION, 'rt').read())
 DEBUG = False
 ALLOWED_HOSTS = [
     # 'localhost',
-    'amazonaws.com',
+    'localhost',
+    '127.0.0.1',
+    '.elasticbeanstalk.com',
+    # 'amazonaws.com',
 ]
 DATABASES = secrets['DATABASES']
 WSGI_APPLICATION = 'config.wsgi.production.application'
@@ -14,5 +17,35 @@ DEFAULT_FILE_STORAGE = 'config.storage.DefaultFileStorage'
 STATICFILES_STORAGE = 'config.storage.StaticFilesStorage'
 
 
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
 # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
