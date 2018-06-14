9111bee5c7a58e966eeed21910df9b7d99677443
hong <hsj2334@gmail.com>
2018-06-14 20:09:48 +0900

    cron job 30분마다 10일치 실행하도록 수정

--- app/crawler/twayair.py
+++ app/crawler/twayair.py
@@ -168,7 +168,8 @@ class TwayData():
             next_day_box = self.driver.find_element_by_xpath("//*[@id='resultbox1']/div[2]/ul/li[5]/a")
             next_day_box.click()
             wait.until(EC.staleness_of(next_day_box))
+            print("---(single day)crawler %s seconds ---" % (time.time() - self.start_time))
 
-        print("---crawler %s seconds ---" % (time.time() - self.start_time))
+        print("---(total day)crawler %s seconds ---" % (time.time() - self.start_time))
         self.driver.close()
         return result
