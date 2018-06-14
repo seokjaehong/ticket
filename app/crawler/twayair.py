52ef5bac7b892190fe6c233a763a06a7e8e81593
hong <hsj2334@gmail.com>
2018-06-14 15:58:31 +0900

    chromedriver install in dockerfile 추가

--- app/crawler/twayair.py
+++ app/crawler/twayair.py
@@ -21,6 +21,7 @@ class TwayData():
     options.add_argument('headless')
     options.add_argument('window-size=1920x1080')
     options.add_argument("disable-gpu")
+    options.add_argument('--no-sandbox')
     driver = webdriver.Chrome('chromedriver', chrome_options=options)
 
     url = "https://www.twayair.com/main.do#;"
