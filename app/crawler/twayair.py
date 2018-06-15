6df2124cdb16d713f660cfd9c4854ff7593582b8
hong <hsj2334@gmail.com>
2018-06-15 14:15:58 +0900

    options.add_argument(disable-dev-shm-usage)

--- app/crawler/twayair.py
+++ app/crawler/twayair.py
@@ -23,6 +23,7 @@ class TwayData():
     options.add_argument("disable-gpu")
     options.add_argument('no-sandbox')
     options.add_argument("--disable-setuid-sandbox")
+    options.add_argument('--disable-dev-shm-usage')
     options.binary_location = '/chromedriver/chromedriver'
     driver = webdriver.Chrome('/chromedriver/chromedriver', chrome_options=options)
 
