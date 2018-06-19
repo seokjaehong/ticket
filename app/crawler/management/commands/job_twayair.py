ed0723b03ddb52a92f186051e51c7f9c8c1fd9ac
SEOKJAE HONG <hsj2334@gmail.com>
2018-06-19 19:10:33 +0900

    day1 change

--- app/crawler/management/commands/job_twayair.py
+++ app/crawler/management/commands/job_twayair.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
         from ticket.models.ticketdata import TicketData
 
         departure_date = date(2018, 8, 1)
-        add_days = 10
+        add_days = 1
         crawler = TwayData()
 
         ticket_data_list = crawler.get_ticket_information(departure_date, add_days)
