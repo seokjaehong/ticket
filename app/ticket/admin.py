08c4175aa04f60c63d856c90ce192f79b91c949e
SEOKJAE HONG <hsj2334@gmail.com>
2018-06-20 18:48:29 +0900

    crawling 주기 변경

--- app/ticket/admin.py
+++ app/ticket/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models.mail_history import MailHistory
 from .models.mail_condition import MailCondition
 from .models.ticketdata import TicketData
 
 
 admin.site.register(TicketData)
 admin.site.register(MailCondition)
+admin.site.register(MailHistory)
