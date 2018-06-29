5553c5b81cdbbc10f99e080ec2d49306b74c9085
SEOKJAE HONG <hsj2334@gmail.com>
2018-06-29 17:55:39 +0900

    mail 기능 완성

--- app/mail/views/list_mailing.py
+++ app/mail/views/list_mailing.py
@@ -1,4 +1,6 @@
+from django.core.mail import EmailMessage
 from django.shortcuts import render
+from django.template.loader import render_to_string
 
 from mail.models import Receiver
 from ticket.models.ticketdata import TicketData
@@ -9,26 +11,39 @@ __all__ = (
 
 
 def list_mailing(request):
-    receiver_lists = Receiver.objects.all()
+    from_mail_address = 'devhsj@gmail.com'
+    title = 'TicketList'
 
-    price_lists=[]
-    for receiver_list in receiver_lists:
-        ticket_query_set = TicketData.objects.filter(
-            origin_place=receiver_list.origin_place,
-            destination_place=receiver_list.destination_place,
-            ticket_price__lte=receiver_list.user_max_price,
-            departure_date=receiver_list.departure_date
-        )
+    context = {}
 
-        for ticket in ticket_query_set:
-            price_lists.append({
-                'mail_address': receiver_list.mail_address,
-                'user_max_price': receiver_list.user_max_price,
-                'departure_date': ticket.departure_date,
-                'departure_datetime': ticket.departure_datetime,
-                'ticket_price': ticket.ticket_price
-            })
-    context = {
-        'price_lists': price_lists
-    }
+    if request.method == "POST":
+        to_mail_address = request.POST['mail_address']
+        receiver_lists = Receiver.objects.filter(mail_address=to_mail_address)
+
+        price_lists = []
+        for receiver_list in receiver_lists:
+            ticket_query_set = TicketData.objects.filter(
+                origin_place=receiver_list.origin_place,
+                destination_place=receiver_list.destination_place,
+                ticket_price__lte=receiver_list.user_max_price,
+                departure_date=receiver_list.departure_date
+            )
+
+            for ticket in ticket_query_set:
+                price_lists.append({
+                    'mail_address': receiver_list.mail_address,
+                    'user_max_price': receiver_list.user_max_price,
+                    'departure_date': ticket.departure_date,
+                    'departure_datetime': ticket.departure_datetime,
+                    'ticket_price': ticket.ticket_price
+                })
+        context = {
+            'price_lists': price_lists
+        }
+        if 'send_mail' in request.POST:
+            contents = render_to_string('mail_form.html', context)
+
+            msg = EmailMessage(title, contents, to=[to_mail_address], from_email=from_mail_address)
+            msg.content_subtype = "html"
+            msg.send()
     return render(request, 'ticket/list.html', context)
